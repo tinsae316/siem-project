f5d6c4dbc23fcde6fa841bda9d9058c7d97d4486
Biniyam Megersa <biniyamhabiebie@gmail.com>
2025-10-06 17:28:42 -0400

    Delete pages directory

--- pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-// pages/_app.tsx
-import '../styles/globals.css'; // ← Tailwind CSS file
-import type { AppProps } from 'next/app';
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
